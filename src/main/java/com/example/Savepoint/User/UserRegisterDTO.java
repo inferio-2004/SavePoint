66caaf5c11c263431e3e55621863cd6c068f0126
Aniruth Narayanan S <saniruth20@gmail.com>
2026-03-12 17:58:06 +0530

    added size constraints to the credentials sent for manual registration in the UserRegisterDTO

--- src/main/java/com/example/Savepoint/User/UserRegisterDTO.java
+++ src/main/java/com/example/Savepoint/User/UserRegisterDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record UserRegisterDTO(@NotBlank String username, @NotBlank @Size(max=72) String password, @NotBlank @Email String mail) {
+public record UserRegisterDTO(@NotBlank @Size(max=50) String username, @NotBlank @Size(min=8, max=72) String password, @NotBlank @Email String mail) {
     public UserRegisterDTO withPassword(String password) {
         return new UserRegisterDTO(this.username, password, this.mail);
     }
